381598721e62e91488e7e1a75d8c6883352a1971
Kirti Sunder Mishra <73066899+realksm@users.noreply.github.com>
2025-12-25 19:08:16 +0530

    Enhance aoc.bat with colors and command options
    Added ANSI color support and command handling.

--- aoc.bat
+++ aoc.bat
@@ -1,8 +1,28 @@
 @echo off
 set MODULE=aoc-2025
 
-echo Compiling...
-javac -cp out\production\%MODULE% -d -cp out\production\%MODULE% src\aoc\Main.java >nul 2>&1
+REM Enable ANSI colors
+for /f %%A in ('echo prompt $E ^| cmd') do set "ESC=%%A"
 
-echo Running...
+REM Colors
+set GREEN=%ESC%[32m
+set CYAN=%ESC%[36m
+set YELLOW=%ESC%[33m
+set RESET=%ESC%[0m
+
+REM Check command
+if "%1"=="gen" goto GEN
+
+REM ===== RUN MODE =====
+echo %CYAN%[AoC] Compiling...%RESET%
+javac -cp out\production\%MODULE% -d out\production\%MODULE% src\aoc\Main.java >nul 2>&1
+
+echo %GREEN%[AoC] Running Day %2%RESET%
 java -cp out\production\%MODULE% aoc.Main %*
+goto END
+
+:GEN
+java -cp out\production\%MODULE% aoc.Main %*
+goto END
+
+:END
